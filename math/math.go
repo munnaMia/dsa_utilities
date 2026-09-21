ffde0c6a0d3722530c94e7313d317e4c8ce216d6
munnaMia <mdmunnamia987@gmail.com>
2026-09-21 21:48:48 +0600

    add: implement factorial, divisor, power functions and prime check

--- math/math.go
+++ math/math.go
@@ -1,9 +1,65 @@
 package math
 
 // Give a number factorial.
-func Factorial (n int) int {
+func Factorial(n int) int {
 	if n == 1 {
 		return n
 	}
 	return n * Factorial(n-1)
+}
+
+func FindAllDivisor1(n int) []int {
+	result := []int{}
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			result = append(result, i)
+		}
+	}
+
+	return result
+}
+
+func FindAllDivisor2(n int) []int {
+	result := []int{}
+	for i := 1; i*i <= n; i++ {
+		if n%i == 0 {
+			if i != n/i {
+
+				result = append(result, i, n/i)
+			} else {
+				result = append(result, i)
+			}
+		}
+	}
+
+	return result
+}
+
+func Pow(x, n int) float64 {
+	var ans float64 = 1
+	pIsPositive := true
+
+	if n < 0 {
+		pIsPositive = false
+		n *= -1
+	}
+
+	if n == 0 {
+		return 1
+	}
+	for n > 0 {
+		if n%2 == 0 {
+			x *= x
+			n /= 2
+		} else {
+			ans *= float64(x)
+			n--
+		}
+	}
+
+	if pIsPositive {
+
+		return ans
+	}
+	return 1 / ans
 }
---
+++ math/prime.go
@@ -0,0 +1,19 @@
+package math
+
+func IsPrime(n int) bool {
+	if n == 2 {
+		return true
+	}
+	if n%2 == 0 || n <= 1 {
+		return false
+	}
+
+	for i := 3; i*i <= n; i += 2 {
+		if n%i == 0 {
+			return false
+		}
+	}
+
+	return true
+
+}
